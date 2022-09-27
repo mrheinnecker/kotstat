2a50daac06e3610f3e9296f9fcd254ef995a9758
mrheinnecker <marco.rheinnecker@gmail.com>
2022-09-27 14:28:51 +0200

    included time to text files

--- main_app.py
+++ main_app.py
@@ -5,14 +5,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+import datetime
 Builder.load_string("""
 <KOT>:
     BoxLayout:
         TextInput:
-            id: txtinp
+            id: time
             text: 'jou'
-            size_hint: (0.2, 0.06)
+            size_hint: (0.2, 0.5)
             cursor_blink: True
             font_size: 10
             multiline: False
@@ -22,10 +22,17 @@ Builder.load_string("""
         Button:
             text: 'enter'
             on_press: 
-                root.on_press_button(txtinp.text)
+                root.on_press_button()
 
 <FOOD>:
     BoxLayout:
+        TextInput:
+            id: time
+            text: ''
+            size_hint: (0.2, 0.5)
+            cursor_blink: True
+            font_size: 10
+            multiline: False
         TextInput:
             id: txtinp
             text: 'jou'
@@ -38,24 +45,32 @@ Builder.load_string("""
             text: 'Goto kot'
             size_hint: (0.1, 0.1)
             pos_hint: {"center_x": 0, "center_y": 0.9}
-            on_press: root.manager.current = 'kot'
+            on_press: root.manager.current = 'kot'            
         Button:
             text: 'enter'
             size_hint: (0.1, 0.1)
             pos_hint: {"center_x": 0.5, "center_y": 0.4}
             on_press: 
                 root.on_press_button(txtinp.text)
+            on_press: root.get_time()
 """)
 
 # Declare both screens
 class KOT(Screen):
-    def on_press_button(self, inp):
-        print(inp)
+    def on_press_button(self):
+        print("hello")
+
+    def get_time(self):
+        return datetime.datetime.now()
+
     pass
 
 class FOOD(Screen):
     def on_press_button(self, inp):
         print(inp)
+    def get_time(self):
+        now = datetime.datetime.now()
+        self.ids.time.text = str(now)
     pass
 
 class TestApp(App):
