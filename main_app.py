b0cdf3eb3a240e7d3fa001b1745e1812811eca07
mrheinnecker <marco.rheinnecker@gmail.com>
2022-09-28 16:20:18 +0200

    finished buttons for adjusting time

--- main_app.py
+++ main_app.py
@@ -30,8 +30,10 @@ Builder.load_string("""
 <FOOD>:
     on_enter: root.get_time()
     BoxLayout:
-        orientation: 'vertical'
+        id: master_screen
+        orientation: 'vertical'  
         BoxLayout:
+            id: time_sign
             orientation: 'horizontal' 
             TextInput:
                 id: day
@@ -70,27 +72,39 @@ Builder.load_string("""
             Button:
                 text: '+ 1 day'
                 size_hint: (0.1, 1)
-                pos_hint: { 'left' : 0}     
+                pos_hint: { 'left' : 0}  
+                on_press:
+                    root.change_time(86400, day.text, month.text, year.text, time.text)   
             Button:
                 text: '- 1 day'
                 size_hint: (0.1, 1)
                 pos_hint: { 'left' : 0.1}   
+                on_press:
+                    root.change_time(-86400, day.text, month.text, year.text, time.text) 
             Button:
                 text: '+ 1h'
                 size_hint: (0.2, 1)
-                pos_hint: { 'left' : 0.2}                                
+                pos_hint: { 'left' : 0.2}  
+                on_press:
+                    root.change_time(3600, day.text, month.text, year.text, time.text)                               
             Button:
                 text: '- 1h'
                 size_hint: (0.2, 1)
-                pos_hint: { 'left' : 0.4} 
+                pos_hint: { 'left' : 0.4}
+                on_press:
+                    root.change_time(-3600, day.text, month.text, year.text, time.text)  
             Button:
                 text: '+ 30 min'
                 size_hint: (0.2, 1)
-                pos_hint: { 'left' : 0.6}                                
+                pos_hint: { 'left' : 0.6}
+                on_press:
+                    root.change_time(1800, day.text, month.text, year.text, time.text)                                 
             Button:
                 text: '- 30 min'
                 size_hint: (0.2, 1)
-                pos_hint: { 'left' : 0.8}  
+                pos_hint: { 'left' : 0.8} 
+                on_press:
+                    root.change_time(-1800, day.text, month.text, year.text, time.text)  
         BoxLayout:
             orientation: 'horizontal' 
             TextInput:
@@ -103,7 +117,7 @@ Builder.load_string("""
                 multiline: False    
             TextInput:
                 id: amount
-                text: ''
+                text: '1'
                 size_hint: (0.2, 1)
                 pos_hint: { 'left' : 0.8}
                 cursor_blink: True
@@ -112,11 +126,15 @@ Builder.load_string("""
             Button:
                 text: '+'
                 size_hint: (0.15, 1)
-                pos_hint: { 'left' : 0.7}                                
+                pos_hint: { 'left' : 0.7}   
+                on_press: 
+                    root.change_amount("+", amount.text)                             
             Button:
                 text: '-'
                 size_hint: (0.15, 1)
                 pos_hint: { 'left' : 0.85}  
+                on_press: 
+                    root.change_amount("-", amount.text)  
         BoxLayout:
             orientation: 'horizontal'
             size_hint: (1, 3)  
@@ -152,22 +170,39 @@ class FOOD(Screen):
     def on_press_button(self, inp):
         print(inp)
     def get_time(self):
-        now = str(datetime.datetime.now()).split(" ")
-        date=now[0]
-        hour = int(now[1].split(":")[0])
-        minute = round(round(int(now[1].split(":")[1])*(5/3)/50)*50/(5/3))
-        if minute == 60:
-            hour = hour+1
-            minute = 0
-        if hour >= 24:
-            hour = hour - 24
-        if minute == 0:
-            minstr = "00"
+
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute_rounded = round(round(now.minute*(5/3)/50)*50/(5/3))
+        time_rounded = now - datetime.timedelta(minutes=now.minute) + datetime.timedelta(minutes=minute_rounded)
+
+        self.ids.time.text = str(time_rounded.hour) + ":" + str(time_rounded.minute)
+        self.ids.day.text = str(time_rounded.day)
+        self.ids.month.text = str(time_rounded.month)
+        self.ids.year.text = str(time_rounded.year)
+    def change_time(self, amount, current_day, current_month, current_year, current_time):
+
+        current_hour=current_time.split(":")[0]
+        current_minute = current_time.split(":")[1]
+        current_date=datetime.datetime(year=int(current_year),
+                                        month=int(current_month),
+                                        day=int(current_day),
+                                        hour=int(current_hour),
+                                        minute=int(current_minute))
+        new_date = current_date + datetime.timedelta(seconds=int(amount))
+        self.ids.time.text = str(new_date.hour) + ":" + str(new_date.minute)
+        self.ids.day.text = str(new_date.day)
+        self.ids.month.text = str(new_date.month)
+        self.ids.year.text = str(new_date.year)
+
+    def change_amount(self, dir, current):
+        if dir == "+":
+            new = float(current)+0.5
         else:
-            minstr = str(minute)
-        time=str(hour)+":"+minstr
-        self.ids.time.text = time
-        #self.ids.date.text = date
+            new = float(current)-0.5
+        if new<0:
+            new = 0
+        self.ids.amount.text = str(new)
     pass
 
 class TestApp(App):
